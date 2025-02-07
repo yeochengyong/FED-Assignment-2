4b0d346d45d220f6cdcf49e2afaa31acdd62cc67
k4nzuu <s10259070@connect.np.edu.sg>
2025-02-07 20:48:23 +0800

    list.html, sneakers.html, clothing.html, fixed some other issues

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-// carousel category swiper
-document.addEventListener("DOMContentLoaded", function () {
-    var swiperCategories = new Swiper(".categories-container", {
-        slidesPerView: "auto", // Ensure slides retain their size
-        spaceBetween: 10, // Reduce spacing without affecting border size
-        centeredSlides: false, // Prevent excessive spacing
-        loop: false, // Ensure natural positioning
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: false,
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: "auto",
-                spaceBetween: 10, // Less space between each item
-            },
-            1024: {
-                slidesPerView: "auto",
-                spaceBetween: 10, // Adjust spacing for medium screens
-            },
-            1440: {
-                slidesPerView: 5, // Ensure exactly 5 items are visible
-                spaceBetween: 10, // Keep spacing minimal
-            }
-        }
-    });
-});
-
+function navigateToCategory(categoryPage) {
+    window.location.href = `/categories/${categoryPage}`;
+}
 // update log in with account page with sidebar
 document.addEventListener('DOMContentLoaded', () => {
     // Check if a user is logged in (saved in localStorage)
